e41d1073d59faf5205b2179123817a798d1f8d2a
MacBaine <kolotkov93@gmail.com>
2024-06-03 20:11:06 +0300

    готово

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,12 +1,17 @@
 package main
 
+import "fmt"
+
 func Reverse(s string) (result string) {
 	for _, v := range s {
 		result = string(v) + result
 	}
-	return
+	return result
 }
 
 func main() {
-	// Place your code here.
+	//скрипт не запускается
+	// получаю ошибку: golang.org/toolchain@v0.0.1-go1.22.windows-amd64: Get "https://proxy.golang.org/golang.org/toolchain/@v/v0.0.1-go1.22.windows-amd64.zip": proxyconnect tcp: dial tcp [::1]:80: connectex: No connection could be made because the target machine actively refused it.
+	//go.mod не менял (не понял суть)
+	fmt.Printf(Reverse("Hello, OTUS!"))
 }

30fbf8d3bd72b10ec4ef8b7d3c8e04dfaae4de70
Igor Tiulkanov <Igor_Tiulkanov@epam.com>
2024-06-04 18:07:31 +0300

    fix lint / tests

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,4 +1,5 @@
 module github.com/KolotkovLD/go_hw/hw01_hello_otus
 
-go 1.21
+go 1.22
 
+require golang.org/x/example/hello v0.0.0-20240205180059-32022caedd6a
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,17 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func Reverse(s string) (result string) {
-	for _, v := range s {
-		result = string(v) + result
-	}
-	return result
-}
+	"golang.org/x/example/hello/reverse"
+)
 
 func main() {
-	//скрипт не запускается
-	// получаю ошибку: golang.org/toolchain@v0.0.1-go1.22.windows-amd64: Get "https://proxy.golang.org/golang.org/toolchain/@v/v0.0.1-go1.22.windows-amd64.zip": proxyconnect tcp: dial tcp [::1]:80: connectex: No connection could be made because the target machine actively refused it.
-	//go.mod не менял (не понял суть)
-	fmt.Printf(Reverse("Hello, OTUS!"))
+	fmt.Println(reverse.String("Hello, OTUS!"))
 }

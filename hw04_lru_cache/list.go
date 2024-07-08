601945a8aaf08fc8ba63be4e642f367e2530766a
MacBaine <kolotkov93@gmail.com>
2024-07-08 18:21:47 +0300

    задел для Cashe

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -91,3 +91,88 @@ func (l *list) MoveToFront(i *ListItem) {
 func NewList() List {
 	return &list{}
 }
+
+//type Cashe interface {
+//	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу.
+//	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу.
+//	Clear()                              // Очистить кэш.
+//}
+//
+//type CasheItem struct {
+//	capasity int
+//	order &list
+//	keys []string
+//}
+
+//// Key - тип ключа для кэша
+//type CKey string
+//
+//// Value - обобщенный тип значения
+//type Value interface{}
+//
+//// CacheItem - структура для элемента кэша
+//type CacheItem struct {
+//	Key   CKey   // ключ
+//	Value Value // значение
+//}
+//
+//// Cache - структура для представления кэша
+//type Cache struct {
+//	Capacity int // ёмкость кэша
+//	mu       sync.Mutex // мьютекс для синхронизации доступа к кэшу
+//	list     *List // список последних использованных элементов
+//	cmap      map[Key]*ListItem // словарь ключ-элемент
+//}
+//
+//// NewCache - функция для создания нового экземпляра кэша
+//func NewCache(capacity int) *Cache {
+//	return &Cache{Capacity: capacity, list: NewList(), cmap: make(cmap[Key]*ListItem)}
+//}
+//
+//// Set - метод для добавления значения в кэш по ключу
+//func (c *Cache) Set(key Key, value Value) bool {
+//	c.mu.Lock() // блокируем мьютекс для синхронизации
+//	defer c.mu.Unlock() // освобождаем мьютекс после выполнения операции
+//
+//	item, exists := c.cmap[key]
+//	if !exists {
+//		item = c.list.PushFront(CacheItem{Key: key, Value: value})
+//		c.cmap[key] = item
+//		if c.list.Len() > c.Capacity {
+//			// если размер списка превышает емкость кэша, удаляем последний элемент
+//			c.list.Remove(c.list.Back())
+//			delete(c.cmap, c.list.Back().Value.Key)
+//		}
+//		return true
+//	}
+//
+//	// если элемент уже существует, обновляем его значение и перемещаем в начало списка
+//	item.Value = value
+//	c.list.MoveToFront(item)
+//	return false
+//}
+//
+//// Get - метод для получения значения из кэша по ключу
+//func (c *Cache) Get(key Key) (Value, bool) {
+//	c.mu.Lock()
+//	defer c.mu.Unlock()
+//
+//	item, exists := c.cmap[key]
+//	if exists {
+//		c.list.MoveToFront(item)
+//		return item.Value, true
+//	}
+//	return nil, false
+//}
+//
+//// Clear - метод для очистки кэша
+//func (c *Cache) Clear() {
+//	c.mu.Lock()
+//	defer c.mu.Unlock()
+//
+//	for _, item := range c.list.List {
+//		delete(c.cmap, item.Value.Key)
+//	}
+//	c.cmap = make(cmap[Key]*ListItem)
+//	c.list = NewList()
+//}

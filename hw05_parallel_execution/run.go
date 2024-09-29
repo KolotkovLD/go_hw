d9ef5af7ffa3da9985a23db430cc29ba825fcefc
MacBaine <kolotkov93@gmail.com>
2024-09-29 19:24:09 +0300

    рутина с обработкой ошибок

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,63 +2,79 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
-var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var uErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Initialize channels for tasks and errors.
-	taskChannel := make(chan Task, len(tasks))
-	errorChannel := make(chan error, m)
-
-	// Count the total number of tasks.
-	totalTasks := len(tasks)
-
-	// Channel to signal completion of tasks.
-	completionSignal := make(chan struct{})
-
-	// Track how many errors have occurred so far.
-	errorCounter := 0
-
-	// Goroutine to handle the processing of jobs.
-	go func() {
-		for job := range taskChannel {
-			go func() {
-				err := job()
-				if err != nil {
-					errorChannel <- err
-					errorCounter++
-					if errorCounter >= m {
-						completionSignal <- struct{}{}
-						return
-					}
-				}
-			}()
-		}
-	}()
-
-	// Close the task channel when all tasks have been sent.
-	go func() {
-		for i := 0; i < totalTasks; i++ {
-			taskChannel <- tasks[i]
-		}
-		close(taskChannel)
-	}()
-
-	// Wait for either all tasks to complete or for the error counter to reach the limit.
-	select {
-	case <-completionSignal:
-		return errors.New("maximum error count exceeded")
-	default:
-		<-completionSignal
+	if m <= 0 {
+		m = len(tasks) + 1
 	}
 
-	// Return an error if any were encountered during execution.
-	if errorCounter > 0 {
-		return errors.New("errors occurred during execution")
+	var (
+		wg         sync.WaitGroup
+		errorCount int
+		errorChan  = make(chan error, len(tasks))
+		doneChan   = make(chan struct{})
+		stopChan   = make(chan struct{})
+		taskChan   = make(chan Task, len(tasks))
+	)
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case task, ok := <-taskChan:
+					if !ok {
+						return
+					}
+					if err := task(); err != nil {
+						errorChan <- err
+					}
+				case <-stopChan:
+					return
+				}
+			}
+		}()
+	}
+
+	// Заполняем канал заданий
+	go func() {
+		defer close(taskChan)
+		for _, task := range tasks {
+			select {
+			case taskChan <- task:
+			case <-stopChan:
+				return
+			}
+		}
+	}()
+
+	// Обрабатываем ошибки
+	go func() {
+		defer close(doneChan)
+		for err := range errorChan {
+			if err != nil {
+				errorCount++
+				if errorCount >= m {
+					close(stopChan)
+					return
+				}
+			}
+		}
+	}()
+
+	wg.Wait()
+	close(errorChan)
+	<-doneChan
+	if errorCount >= m {
+		return ErrErrorsLimitExceeded
 	}
 
 	return nil

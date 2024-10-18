7921f39975513b4e150fb928e3219bd7c7b6e190
MacBaine <kolotkov93@gmail.com>
2024-10-18 19:02:18 +0300

    draft

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +12,44 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+//func Copy(fromPath, toPath string, offset, limit int64) error {
+//	// Place your code here.
+//	return nil
+//}
+
+func Copy(from, to string, offset, limit int64) error {
+	src, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	fi, err := src.Stat()
+	if err != nil {
+		return err
+	}
+	if offset > fi.Size() {
+		return fmt.Errorf("offset is larger than filesize \n")
+	}
+
+	if limit == 0 || offset+limit > fi.Size() {
+		limit = fi.Size() - offset
+	}
+
+	_, err = src.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	dst, err := os.Create(to)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	//bar := progressbar.DefaultBytes(limit, "Copying")
+	_, err = io.Copy(io.MultiWriter(dst), io.LimitReader(src, limit)) // , bar
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
 )
 
-var (
-	from, to      string
-	limit, offset int64
-)
-
-func init() {
-	flag.StringVar(&from, "from", "", "file to read from")
-	flag.StringVar(&to, "to", "", "file to write to")
-	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
-	flag.Int64Var(&offset, "offset", 0, "offset in input file")
-}
+//var (
+//	from, to      string
+//	limit, offset int64
+//)
+//
+//func init() {
+//	flag.StringVar(&from, "from", "", "file to read from")
+//	flag.StringVar(&to, "to", "", "file to write to")
+//	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
+//	flag.Int64Var(&offset, "offset", 0, "offset in input file")
+//}
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+	from := flag.String("from", "", "Path to source file")
+	to := flag.String("to", "", "Path to destination file")
+	offset := flag.Int64("offset", 0, "Offset in source file")
+	limit := flag.Int64("limit", 0, "Number of bytes to copy (0 means entire file)")
+
+	if *from == "" || *to == "" {
+		fmt.Println("Both -from and to arguments are requred")
+		flag.Usage()
+		os.Exit(1)
+	}
+	err := Copy(*from, *to, *offset, *limit)
+	if err != nil {
+		log.Fatalf("ERROR: %v\n", err)
+	}
+	fmt.Printf("File %s copied to %s\n", *from, *to)
+
 }

f49bfa3b85776cea512e31813d11284138a59bd9
MacBaine <kolotkov93@gmail.com>
2024-11-01 20:11:47 +0300

    new test to no limit of errors

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -68,7 +68,7 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
 
-	t.Run("tasks without errors", func(t *testing.T) {
+	t.Run("tasks without error limit", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 

87e54a38f90b7229e33847b21be48d1d61694ef9
MacBaine <kolotkov93@gmail.com>
2024-11-01 20:40:14 +0300

    N+M tasks done

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -30,11 +30,11 @@ func Run(tasks []Task, n, m int) error {
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go runTask(&wg, taskChan, &errorCount, &runTasksCount, i)
+		go runTask(&wg, taskChan, &errorCount, &runTasksCount, i, n, m)
 	}
 
 	wg.Wait()
-	if atomic.LoadInt32(&errorCount) >= int32(m) && (int32(n)+int32(m)) <= atomic.LoadInt32(&runTasksCount) {
+	if stopRule(&errorCount, &runTasksCount, n, m) {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
@@ -45,6 +45,8 @@ func runTask(wg *sync.WaitGroup,
 	errorCount *int32,
 	runTasksCount *int32,
 	workerID int,
+	n int,
+	m int,
 ) {
 	// Запускает таски из канала taskChan
 	defer wg.Done()
@@ -53,7 +55,7 @@ func runTask(wg *sync.WaitGroup,
 		task, ok := <-taskChan
 		atomic.AddInt32(runTasksCount, 1)
 		log.Printf("Goroutine %d: received a task, runTasksCount: %d\n", workerID, *runTasksCount)
-		if !ok {
+		if !ok || stopRule(errorCount, runTasksCount, n, m) {
 			log.Printf("Goroutine %d: taskChan closed, exiting\n", workerID)
 			return
 		}
@@ -78,10 +80,18 @@ func sendTasks(taskChan chan Task,
 		log.Printf("sendTasks is done!!!!!!!")
 	}()
 	for _, task := range tasks {
-		if atomic.LoadInt32(errorCount) >= int32(m) && (int32(n)+int32(m)) <= atomic.LoadInt32(runTasksCount) {
-			log.Printf(" [sendTasks] >=m   errorCount: %d\n", *errorCount)
+		if stopRule(errorCount, runTasksCount, n, m) {
+			log.Printf("     [sendTasks]  errorCount: %d ; runTasksCount: %d\n", *errorCount, *runTasksCount)
 			return
 		}
 		taskChan <- task
 	}
+}
+
+func stopRule(errorCount *int32, runTasksCount *int32, n, m int) bool {
+	if atomic.LoadInt32(errorCount) >= int32(m) && (int32(n)+int32(m)) <= atomic.LoadInt32(runTasksCount) {
+		log.Printf(" [stop_rule]  errorCount: %d ; runTasksCount: %d\n", *errorCount, *runTasksCount)
+		return true
+	}
+	return false
 }

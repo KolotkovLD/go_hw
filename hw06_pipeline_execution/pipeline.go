774c5ab21777a694572545a116b688d9da905e3c
MacBaine <kolotkov93@gmail.com>
2024-11-06 12:01:33 +0300

    draft

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,6 +8,12 @@ type (
 
 type Stage func(in In) (out Out)
 
+//func Stage(in <-chan interface{}) (out <-chan interface{}) {
+//	out = make(chan interface{})
+//	go func() { /* Some work */ }()
+//	return out
+//}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
 	return nil
